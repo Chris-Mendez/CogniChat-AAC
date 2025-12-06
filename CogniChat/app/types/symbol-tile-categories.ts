8b967bd9c55c56d6b98411c0aee935a17993c380
Aaron Maryniewski <aa526825@ucf.edu>
2025-12-06 12:52:36 -0500

    clarify symbol tile category documentation

--- CogniChat/app/types/symbol-tile-categories.ts
+++ CogniChat/app/types/symbol-tile-categories.ts
@@ -2,9 +2,6 @@
  * Enumeration of all available symbol tile categories.
  *
  * @remarks
- * Categories are hardcoded; users cannot add or remove them. They
- * can, however, change the colors of the categories. Each
- * key corresponds to a default entry in the "categories" map.
- * This key is for internal use.
+ * Categories are hardcoded; users cannot add or remove them.
  */
 export type SymbolTileCategoryKey = "other" | "noun" | "adjective" | "verb";
